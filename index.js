dac1ccda2507e3121ad116c7705e2f7bd9dbb360
Sarah Nadler <sarah.m.nadler@gmail.com>
2017-11-10 02:03:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 function theBeatlesPlay(musicians, instruments){
   var array = [];
   for(let i=0; i < musicians.length; i++){
-    for(let j=0; j < instruments.length; j++){
       console.log(musicians[i] + " plays the " + instruments[j]);
-    }
   }
 }
 
